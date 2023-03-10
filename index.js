fd46a5dd994788bae9650834c40749ef4541f4e6
AlavarCode <111548209+AlavarCode@users.noreply.github.com>
2023-03-10 15:47:58 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+export function saludarEstudiantes(array) {
+    for (let estudiante of array) {
+        console.log("Hola "+estudiante);
+    } return undefined;
+}
+
+// Este es un comentario
+let estudiantes = ["Alvaro", "Juan", "María"];
+
+saludarEstudiantes(estudiantes);
